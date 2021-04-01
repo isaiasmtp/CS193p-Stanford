cd8db0a01a8359099cae7023ec013cf93e925d4c
Isaias Martins <isaias.pontes@ccc.ufcg.edu.br>
2021-04-01 20:58:25 -0300

    Implementing MVVM

---
+++ Memorize/Memorize/MemoryGame.swift
@@ -0,0 +1,34 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Isaias Pontes on 01/04/21.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    
+    var cards: Array<Card>
+    
+    func choose(card: Card) {
+        print("card chosen: \(card)")
+    }
+    
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent){
+        cards = Array<Card>()
+        
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(content: content, id: pairIndex*2))
+            cards.append(Card(content: content, id: pairIndex*2 + 1))
+        }
+    }
+    
+    struct Card: Identifiable {
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
+        var content: CardContent
+        var id: Int
+    }
+}
