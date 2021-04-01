0c706a8cd8daa4db37ff481cc76230d0f5f72b27
Isaias Martins <isaias.pontes@ccc.ufcg.edu.br>
2021-04-01 15:21:06 -0300

    Creating project

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Isaias Martins on 05/03/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
