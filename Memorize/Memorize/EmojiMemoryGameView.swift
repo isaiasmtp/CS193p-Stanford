4e090cb817a123b05a719836dbf0366d9f35268e
Isaias Martins <isaias.pontes@ccc.ufcg.edu.br>
2021-04-06 17:28:17 -0300

    Implement Shape Pie

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -32,11 +32,13 @@ struct CardView : View {
         }
     }
     
-    func body(for size: CGSize) -> some View{
+    private func body(for size: CGSize) -> some View{
         ZStack{
             if card.isFaceUp {
                 RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
                 RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true)
+                    .padding(5).opacity(0.4)
                 Text(card.content)
             }else{
                 if !card.isMatched {
@@ -49,16 +51,17 @@ struct CardView : View {
     
     //MARK: - Drawing Constants
      
-    let cornerRadius: CGFloat = 10.0
-    let edgeLineWidth: CGFloat = 3
-    
-    func fontSize(for size: CGSize) -> CGFloat{
-       return min(size.width, size.height) * 0.75
-    }
+    private let cornerRadius: CGFloat = 10.0
+    private let edgeLineWidth: CGFloat = 3
+    private func fontSize(for size: CGSize) -> CGFloat{ min(size.width, size.height) * 0.7 }
 }
 
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        EmojiMemoryGameView(viewModel: EmojiMemoryGame())
-//    }
-//}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        let game =  EmojiMemoryGame()
+        game.choose(card: game.cards[0])
+        game.choose(card: game.cards[2])
+        return EmojiMemoryGameView(viewModel: game)
+    }
+}
