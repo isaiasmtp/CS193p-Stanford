03a2a88a56aa511ccf43d6829c17eb67e27ab7c0
Isaias Martins <isaias.pontes@ccc.ufcg.edu.br>
2021-04-07 18:49:49 -0300

    Implement Animation and calculate bonus time

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -11,14 +11,25 @@ struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
     
     var body: some View {
-        Grid(viewModel.cards){ card in
-            CardView(card: card).onTapGesture {
-                viewModel.choose(card: card)
+            VStack{
+            Grid(viewModel.cards){ card in
+                CardView(card: card).onTapGesture {
+                    withAnimation(.linear(duration: 0.5)){
+                        viewModel.choose(card: card)
+                    }
+                }
+                .padding(5)
             }
-            .padding(5)
+            .padding()
+            .foregroundColor(Color.orange)
+            Button( action: {
+                    withAnimation(.easeInOut){
+                        viewModel.resetGame()
+                    }
+                },
+                label: { Text("New Game") }
+            )
         }
-        .padding()
-        .foregroundColor(Color.orange)
     }
 }
 
@@ -32,22 +43,45 @@ struct CardView : View {
         }
     }
     
+    @State private var animatedBonusRemaing: Double = 0
+    
+    private func startBonusTimeAnimation() {
+        animatedBonusRemaing = card.bonusRemaining
+        withAnimation(.linear(duration: card.bonusTimeRemaining)){
+            animatedBonusRemaing = 0
+        }
+    }
+    
     @ViewBuilder
     private func body(for size: CGSize) -> some View{
         if card.isFaceUp || !card.isMatched {
             ZStack{
-                Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true)
-                    .padding(5).opacity(0.4)
+                Group{
+                    if card.isConsumingBonusTime{
+                        Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(-animatedBonusRemaing*360-90), clockwise: true)
+                            .onAppear{
+                                startBonusTimeAnimation()
+                            }
+                    } else {
+                        Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(-card.bonusRemaining*360-90), clockwise: true)
+                    }
+                }
+                .padding(5).opacity(0.4)
+
                 Text(card.content)
                     .font(Font.system(size: fontSize(for: size)))
+                    .rotationEffect(Angle.degrees(card.isMatched ? 360: 0))
+                    .animation(card.isMatched ? Animation.linear(duration: 1).repeatForever(autoreverses: false) : .default)
             }
             .cardify(isFaceUp: card.isFaceUp)
+            .transition(.scale)
         }
     }
     
     //MARK: - Drawing Constants
      
     private func fontSize(for size: CGSize) -> CGFloat{ min(size.width, size.height) * 0.7 }
+    
 }
 
 
--- Memorize/Memorize/MemoryGame.swift
+++ Memorize/Memorize/MemoryGame.swift
@@ -38,18 +38,86 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
     
     init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent){
         cards = Array<Card>()
-        
         for pairIndex in 0..<numberOfPairsOfCards {
             let content = cardContentFactory(pairIndex)
             cards.append(Card(content: content, id: pairIndex*2))
             cards.append(Card(content: content, id: pairIndex*2 + 1))
         }
+        cards.shuffle()
     }
     
     struct Card: Identifiable {
-        var isFaceUp: Bool = false
-        var isMatched: Bool = false
+        var isFaceUp: Bool = false {
+            didSet {
+                if isFaceUp {
+                    startUsingBonusTime()
+                } else {
+                    stopUsingBonusTime()
+                }
+            }
+        }
+        
+        var isMatched: Bool = false {
+            didSet {
+                stopUsingBonusTime()
+            }
+        }
+        
         var content: CardContent
         var id: Int
+    
+        
+        // MARK: - Bonus Time
+        
+        // zero means "no bonus available" for this card
+        var bonusTimeLimit : TimeInterval = 6
+        
+        // last time this card was turned face up
+        var lastFaceUpDate : Date?
+        
+        // accumulated time this card has been face up
+        var pastFaceUpTime: TimeInterval = 0
+        
+        // how long this card has ever been face up
+        private var faceUptTime: TimeInterval {
+            if let lastFaceUpDate = self.lastFaceUpDate {
+                return pastFaceUpTime + Date().timeIntervalSince(lastFaceUpDate)
+            } else {
+                return pastFaceUpTime
+            }
+        }
+        
+        // how much time left before the bonus runs out
+        var bonusTimeRemaining: TimeInterval {
+            max(0, bonusTimeLimit - faceUptTime)
+        }
+        
+        // percentage of the bonus time remaining
+        var bonusRemaining: Double {
+            (bonusTimeLimit > 0 && bonusTimeRemaining > 0) ? bonusTimeRemaining/bonusTimeLimit : 0
+        }
+        
+        // wheter the card was matched during the bonus time period
+        var hasEarnedBonus : Bool {
+            isMatched && bonusTimeRemaining > 0
+        }
+        
+        // wheter isFaceUp, unmatched and have not yet used up the bonus window
+        var isConsumingBonusTime: Bool {
+            isFaceUp && !isMatched && bonusTimeRemaining > 0
+        }
+        
+        // called when the card transitions to face up state
+        private mutating func startUsingBonusTime() {
+            if isConsumingBonusTime, lastFaceUpDate == nil {
+                lastFaceUpDate = Date()
+            }
+        }
+        
+        // called when the card goes back face down or gets matched
+        private mutating func stopUsingBonusTime() {
+            pastFaceUpTime = faceUptTime
+            lastFaceUpDate = nil
+        }
     }
 }
